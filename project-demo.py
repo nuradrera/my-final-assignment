e0c44d1fee73cc0611c72ca499fdf90f019d2a74
nuradrera <107099106+nuradrera@users.noreply.github.com>
2022-06-14 17:12:15 +0800

    Update project-demo.py

--- project-demo.py
+++ project-demo.py
@@ -8,7 +8,7 @@ st.write("""
 This app predicts the **Iris flower** type!
 """)
 
-image = Image.open('iris%20image.png')
+image = Image.open('iris_image.png')
 st.image(image,'Iris Virginica Species')
 
 st.sidebar.header('User Input Parameters')
